a2b91718a5a1ec893b101742a3c96a24fca89379
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-04 21:59:00 +0700

    node

--- 0053_Doublelinkedlist.cpp
+++ 0053_Doublelinkedlist.cpp
@@ -74,6 +74,10 @@ bool search(int rollNo, Node** previous, Node** current)
 
 void deleteNode()
 {
+	Node* previous, * current;
+	int rollNo;
 
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo;
 }
 
