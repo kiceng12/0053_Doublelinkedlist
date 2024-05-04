d7accb121e1cfe2ac6ba2202ff12cc7ffbc15f10
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-04 22:11:52 +0700

    BOOL LISTEMPTY

--- 0053_Doublelinkedlist.cpp
+++ 0053_Doublelinkedlist.cpp
@@ -117,9 +117,11 @@ void deleteNode()
 			current->next->prev = previous;
 		}
 	}
+
 	delete current;
 	cout << "\x1b[32mRecord with roll number " << rollNo << " delete\x1b[0m" << endl;
 }
 
+bool listEmpty()
 
 
