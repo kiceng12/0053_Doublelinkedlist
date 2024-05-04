9e1939c828477f471a0d2ee5efba349d7c3b5490
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-04 21:43:28 +0700

    void addnode

--- 0053_Doublelinkedlist.cpp
+++ 0053_Doublelinkedlist.cpp
@@ -10,5 +10,12 @@ struct Node
 	Node* prev;
 };
 
+Node* START = NULL;
 
-
+void addNode() {
+	Node* newNode = new Node();
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs;
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name;
+}
